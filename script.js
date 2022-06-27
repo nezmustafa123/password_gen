b1d565d1a4be62c04e543823542983ed054c49a5
Nez Mustafa <nezmustafa123@gmail.com>
2022-06-27 16:24:24 +0100

    create variables and get elements

---
+++ script.js
@@ -0,0 +1,13 @@
+const empty = "",
+  uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lCase = "abcdefghijklmnopqrstuvwxyz",
+  number = "0123456789",
+  symbol = "!@#$%^&*=-_";
+
+const pLength = document.getElementById("p-length"),
+  upperCase = document.getElementById("p-uppercase"),
+  lowerCase = document.getElementById("p-lowercase"),
+  pNumber = document.getElementById("p-number"),
+  pSymbol = document.getElementById("p-symbol"),
+  submit = document.getElementById("submit"),
+  password = document.getElementById("password");
